0fc0f8cfeffb7d093cc30260b48640381eec0044
Eugene Krasnoshlykov <UncleShneerson@yandex.ru>
2023-03-12 01:57:43 -0300

    fixig and refactoring for 2nd review

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,44 @@
-let content = document.querySelector('.content');
-let popup = document.querySelector('.popup');
+// ПЕРЕМЕННЫЕ
+let content = document.querySelector('.content'); //Содержимое
+let btnEdit = content.querySelector('.button_func_edit'); //Кнопка редактировать профиль
+let profileName = content.querySelector('.profile__name'); //Имя
+let profileJob = content.querySelector('.profile__description'); //Работа
+let popup = document.querySelector('.popup'); //Попап
+let btnClose = popup.querySelector('.popup__btn-close'); //Кнопка закрыть
+let pForm = document.querySelector('.form'); //Форма профиля
+let nameInput = pForm.querySelector('input[name="pfName"]'); //Поле ввода Имя
+let jobInput = pForm.querySelector('input[name="pfJob"]'); //Поле ввода Работа
 
-let btnEdit = content.querySelector('.button_func_edit');
-let btnClose = popup.querySelector('.popup__btn-close');
 
 
-function popupVisible () {
-  popup.classList.toggle('popup_opened');
+// ФУНКЦИИ
+//Открытие попапа
+function popupVisibleOn () {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  popup.classList.add('popup_opened');
+  console.log(profileJob.textContent + ' ' + nameInput.value);
 }
 
-btnEdit.addEventListener('click', popupVisible);
-btnClose.addEventListener('click', popupVisible);
+
+//Закрытие попапа
+function popupVisibleOff () {
+  popup.classList.remove('popup_opened');
+}
 
 
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__form-input_name');
-let jobInput = document.querySelector('.popup__form-input_job');
-
-let profileName = content.querySelector('.profile__name');
-let profileJob = content.querySelector('.profile__description');
-
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
-
+//Сохранение профайл формы
 function handleFormSubmit (evt) {
-  evt.preventDefault();
+  evt.preventDefault(); //Отменяем стандартную отправку, УДАЛИТЬ в будущем
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
 
-
-  if (nameInput.value.length == 0 || jobInput.value.length == 0) {
-    alert('Пожалуйста, заполните оба поля');
-  } else {
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    popupVisible ();
-  }
+  popupVisibleOff (); //Закрываем попап
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
+
+
+// ОТСЛЕЖИВАНИЯ
+btnEdit.addEventListener('click', popupVisibleOn); //Редактировать профиль
+btnClose.addEventListener('click', popupVisibleOff); //Закрыть редактирование профиля
+pForm.addEventListener('submit', handleFormSubmit); //Сабмит профайл формы
