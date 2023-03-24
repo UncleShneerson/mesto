f7236e05ba869ab96770275fe7dae11f99290ab1
Eugene Krasnoshlykov <UncleShneerson@yandex.ru>
2023-03-24 20:33:51 -0300

    refactoring 2.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,3 @@
-// Сергей, простите, не могу не сказать. Получил комменты, прочитал, не понял практически 2\3 - жутко надулся на весь день от мысли "НУ ВСЕ ЖЕ РАБОТАЕТ!!!!"
-// уже в конце подумал ну надо же что-то делать. Сел, разобрал последовательно. вник. Сижу смеюсь. Не могу не поблагодарить за все комментарии
-// от всех .next.next.child.nex избавился ) потому что изначально чушь передал в галлерею.
-// имена привел в соответствие и понял что от 90% комментариев можно избавиться, т.к. они дублируют имена.
-// В общем надеюсь ничего не пропустил, но жду еще комментарии, т.к. практика показывает, что сдаю ревью раз на 3й )
-//
-// один вопрос. В карточке я делал картинку div'ом, чтобы через padding-bottom 100% заставлять ее всегда быть квадратной при адаптиве, теперь есть 3 варианта
-// 1) для img я нашел выход в aspect-ratio: 1 но он работает только с середины 21-го года.
-// 2) так же понимаю, что в теории можно по средствам js брать ширину картинки при загрузки страницы и передавать это же значение высотой. (но стоит ли грузить этим систему?)
-// 3) отойти от квадрата и задать высоту всегда 250px
-// я выбрал в.1 в надежде, что все обновились, но может надо решить иначе?
-
-
-
-
 // ПЕРЕМЕННЫЕ
 const content = document.querySelector('.content');
 
@@ -21,7 +6,7 @@ const profileJob = content.querySelector('.profile__description');
 
 const btnEdit = content.querySelector('.button_func_edit');
 const btnAdd = content.querySelector('.button_func_add');
-const btnClose = document.querySelectorAll('.popup__btn-close');
+const btnCloseList = document.querySelectorAll('.popup__btn-close');
 
 const popupProfile = document.querySelector('.popup_funct_profile');
 const formProfile = popupProfile.querySelector('.form');
@@ -68,15 +53,14 @@ function createCard (item) {
   //Навешиваем события
   cardImage.addEventListener('click', () => {
     createGallery (item);
-    openPopup(popupGallery);
   });
 
-  cardLike.addEventListener('click', (evt) => {
-    likeIt(evt.target);
+  cardLike.addEventListener('click', () => {
+    likeIt(cardLike);
   });
 
-  cardDelete.addEventListener('click', (evt) => {
-    deleteCard(evt.target);
+  cardDelete.addEventListener('click', () => {
+    deleteCard(cardElement);
     areThereCards();
   });
 
@@ -90,7 +74,7 @@ function likeIt (item) {
 
 
 function deleteCard (item) {
-  item.closest('.places__card').remove()
+  item.remove()
 }
 
 
@@ -98,6 +82,8 @@ function createGallery (item) {
   imageGallery.src = item.link;
   imageGallery.alt = item.name
   captionGallery.textContent = item.name;
+
+  openPopup(popupGallery);
 }
 
 
@@ -129,7 +115,7 @@ function editProfile () {
 
 
 function handleSubmitProfileForm (evt) {
-  evt.preventDefault(); //Отменяем стандартную отправку, УДАЛИТЬ в будущем
+  evt.preventDefault(); //Отменяем стандартную отправку
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
 
@@ -139,7 +125,7 @@ function handleSubmitProfileForm (evt) {
 
 //Сабмит формы добавления
 function handleSubmitCardForm (evt) {
-  evt.preventDefault(); //Отменяем стандартную отправку, УДАЛИТЬ в будущем
+  evt.preventDefault(); //Отменяем стандартную отправку
 
   // Получаем данные и передаем в массив, передаем на рендер последний элемент
   const newCardValues = {
@@ -147,11 +133,9 @@ function handleSubmitCardForm (evt) {
     link: inputCardLink.value
   };
 
-  initialCards.push(newCardValues);
-  renderCard(initialCards[initialCards.length - 1]);
+  renderCard(newCardValues);
 
-  inputCardPlace.value = '';
-  inputCardLink.value = '';
+  formCard.reset();
 
   areThereCards ();
   closePopup (popupCard);
@@ -173,6 +157,6 @@ btnAdd.addEventListener('click', () => {openPopup(popupCard);});
 formCard.addEventListener('submit', handleSubmitCardForm);
 
 //Закрытия попапов
-btnClose.forEach((item) => {item.addEventListener('click', (evt) =>
+btnCloseList.forEach((item) => {item.addEventListener('click', (evt) =>
   {closePopup(evt.target.closest('.popup'));});
 });
